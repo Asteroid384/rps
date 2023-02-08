5e6e73ad4671382cbd8db5a96544571540cec86a
Matt Atkins <mattkins99@hotmail.com>
2023-02-07 21:35:51 -0800

    Added Unit Test project

---
+++ src/RPS.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace RPS.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void FakeInitialTest()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
